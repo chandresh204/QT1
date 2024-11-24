53c2723f3d33f72582b1d962b912036c6313a744
chad <204chandresh@gmail.com>
2024-11-24 18:54:03 +0530

    qrencode test

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,12 +1,39 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
+#include <qrencode.h>
 
 MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
+    : QMainWindow(parent),
+    ui(new Ui::MainWindow),
+    display(""),
+    opEvent(0),
+    val1(0),
+    val2(0)
 {
     ui->setupUi(this);
-    connect(ui -> pushButton, &QPushButton::clicked, this, &MainWindow::handleButton);
+    connect(ui->btn_1, &QPushButton::clicked, this, &MainWindow::handleNumKey);
+    connect(ui->btn_2, &QPushButton::clicked, this, &MainWindow::handleNumKey);
+    connect(ui->btn_3, &QPushButton::clicked, this, &MainWindow::handleNumKey);
+    connect(ui->btn_add, &QPushButton::clicked, this, &MainWindow::handlePlusKey);
+    connect(ui->btn_eql, &QPushButton::clicked, this, &MainWindow::handleEqualKey);
+}
+
+QImage MainWindow::createQRCode(const QString &data, int size = 300) {
+    QRcode *qr = QRcode_encodeString(data.toUtf8().constData(), 0, QR_ECLEVEL_Q, QR_MODE_8, 1);
+    if (!qr) return QImage();
+
+    QImage image(qr->width, qr->width, QImage::Format_RGB32);
+    image.fill(Qt::white);
+
+    for (int y = 0; y < qr->width; y++) {
+        for (int x = 0; x < qr->width; x++) {
+            QColor color = (qr->data[y * qr->width + x] & 0x01) ? Qt::black : Qt::white;
+            image.setPixelColor(x, y, color);
+        }
+    }
+
+    QRcode_free(qr);
+    return image.scaled(size, size, Qt::KeepAspectRatio);
 }
 
 
@@ -15,7 +42,28 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-void MainWindow::handleButton()
-{
-    ui->label->setText("this value will change");
+void MainWindow::handleNumKey() {
+    QPushButton *btn = qobject_cast<QPushButton *>(sender());
+    QString str = btn->text();
+    if(opEvent == 0) {
+        display += str;
+        ui->label->setText(display);
+    } else {
+
+    }
+}
+
+void MainWindow::handlePlusKey() {
+    opEvent = 1;
+    ui->op_sign->setText(QString("+"));
+    val1 = val1 + display.toInt();
+    ui->label->setText(QString::number(val1));
+}
+
+void MainWindow::handleEqualKey() {
+    opEvent = 0;
+    val2 = display.toInt();
+    val1 =val1 + val2;
+    ui->label->setText(QString::number(val1));
+    ui->op_sign->setText("=");
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <qrencode.h>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -15,13 +16,19 @@ class MainWindow : public QMainWindow
 
 public:
     MainWindow(QWidget *parent = nullptr);
+    QImage createQRCode(const QString &data, int size);
     ~MainWindow();
 
 private slots:
-    void handleButton();
+    void handleNumKey();
+    void handlePlusKey();
+    void handleEqualKey();
 
 private:
     Ui::MainWindow *ui;
-    int count;
+    QString display;
+    int opEvent; // 0=none, 1=add
+    int val1;
+    int val2;
 };
 #endif // MAINWINDOW_H
